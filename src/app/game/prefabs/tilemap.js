1d36d831ab056b77c918725127ddaee9087b5e67
AngryPixel <none@e.com>
2015-03-15 23:22:51 -0400

    Resources

--- src/app/game/prefabs/tilemap.js
+++ src/app/game/prefabs/tilemap.js
@@ -1,1 +1,33 @@
+(function () {
+    'use strict';
 
+    angular
+        .module('app.Game')
+        .factory('Tilemap', TilemapPrefab);
+
+    TilemapPrefab.$inject = ['$log'];
+
+    function TilemapPrefab ($log){
+        var Tilemap = function (game, key) {
+            Phaser.Tilemap.call(this, game, key);
+
+            var backgroundLayer;
+
+            // add tile sets
+            this.addTilesetImage('tile', 'tile');
+            this.addTilesetImage('happyland', 'happyland');
+            // create map layers, reference the background layer
+            backgroundLayer = this.createLayer('grass');
+            this.createLayer('tree');
+            this.createLayer('object');
+            //resizes the game world to match the layer dimensions
+            backgroundLayer.resizeWorld();
+        };
+
+        Tilemap.prototype = Object.create(Phaser.Tilemap.prototype);
+        Tilemap.prototype.constructor = Tilemap;
+
+        return Tilemap;
+    }
+
+})();
